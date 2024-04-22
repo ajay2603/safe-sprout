4723116605efead96920a5aa7122567274f45a3a
ajay2603 <j.a.r.raju2003@gmail.com>
2024-04-22 23:59:36 +0530

    removed some log statements in backend

--- backend/routes/child.js
+++ backend/routes/child.js
@@ -37,35 +37,29 @@ router.post("/new-child", async (req, res) => {
   const token = req.headers.authorization;
   try {
     try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      console.log(decoded);
-      if (!decoded) {
-        res.status(401).json({ message: "user not autunticated" });
-        return;
-      }
-
-      const _id = decoded.id;
-      const usr = await User.findById(_id);
-      if (!usr) {
-        res.status(404).json({ message: "user not found" });
-        return;
-      } else {
-        const { name } = req.body;
-        const newChild = Child({
-          name,
-          parent: _id,
-        });
-        const ch = await newChild.save();
-        const parent = await User.findById(_id);
-        parent.children.push(ch._id);
-        await parent.save();
-        res
-          .status(200)
-          .json({ message: "child added successfully", id: ch._id });
-      }
+      var decoded = jwt.verify(token, process.env.JWT_SECRET);
     } catch (err) {
       console.error(err);
       res.status(401).json({ message: "user not autunticated" });
+      return;
+    }
+
+    const _id = decoded.id;
+    const usr = await User.findById(_id);
+    if (!usr) {
+      res.status(404).json({ message: "user not found" });
+      return;
+    } else {
+      const { name } = req.body;
+      const newChild = Child({
+        name,
+        parent: _id,
+      });
+      const ch = await newChild.save();
+      const parent = await User.findById(_id);
+      parent.children.push(ch._id);
+      await parent.save();
+      res.status(200).json({ message: "child added successfully", id: ch._id });
     }
   } catch (err) {
     console.error(err);
--- backend/sockets/socket.js
+++ backend/sockets/socket.js
@@ -4,6 +4,7 @@ const initIO = (IO) => {
   this.IO = IO;
   IO.on("connect", (socket) => {
     this.socket = socket;
+    console.log("connected");
     socket.on("disconnect", () => {
       console.log("disconnect");
     });
